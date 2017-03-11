961bdc45c8b24d9972bcfa0aa92bc39ee1cf9741
Glenn Spinks <glennspinks@gmail.com>
2017-03-11 21:41:52 +0800

    Add new components and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,42 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AuthService } from './auth.service';
-import { MaterialModule } from '@angular/material' ;
+import { TaskListService } from './task-list/task-list.service';
+import { MaterialModule } from '@angular/material';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-list/task-form/task-form.component' ;
+
+import { Http, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+export function authHttpFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig(), http, options);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
+    SlimLoadingBarModule.forRoot()
   ],
   providers: [
-    AuthService
+    AuthService,
+    TaskListService, 
+    {
+      provide: AuthHttp,
+      useFactory: authHttpFactory,
+      deps: [ Http, RequestOptions ]
+    }
   ],
   bootstrap: [AppComponent]
 })
