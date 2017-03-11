ca3aaaf493b7d5d8386b989e848d3ce359e3a7b0
Glenn Spinks <glennspinks@gmail.com>
2017-03-11 21:42:25 +0800

    Task list service

---
+++ src/app/task-list/task-list.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthHttp } from 'angular2-jwt';
+
+@Injectable()
+export class TaskListService {
+
+  private static TASKS_ENDPOINT = 'https://wt-f783211be3af258ca3d9442b24ad85b9-0.run.webtask.io/tasks';
+
+  constructor(private authHttp: AuthHttp) { }
+
+  loadTasks$(): Observable<any> {
+    return this.authHttp.get(TaskListService.TASKS_ENDPOINT);
+  }
+
+  addTask$(task): Observable<any> {
+    return this.authHttp.post(TaskListService.TASKS_ENDPOINT, { description: task });
+  }
+
+  deleteTask$(task): Observable<any> {
+    return this.authHttp.delete(TaskListService.TASKS_ENDPOINT + '?id=' + task._id);
+  }
+
+
+
+}
